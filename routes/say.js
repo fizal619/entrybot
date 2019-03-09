c0bce9b6ce19e44992b9dea70c7d134803faaa9c
Fizal Sarif <fizal.sarif@gmail.com>
2019-03-09 15:03:04 -0500

    fix tts

--- routes/say.js
+++ routes/say.js
@@ -1,6 +1,7 @@
 const googleTTS = require("google-tts-api");
 const fs = require("fs");
 const path = require('path');
+const axios = require('axios');
 
 const isDevChannel = (voiceChannel) => {
   return voiceChannel && process.env.NODE_ENV !== 'development' && voiceChannel.name === 'entrybot-development';
@@ -8,20 +9,33 @@ const isDevChannel = (voiceChannel) => {
 
 
 module.exports = async (msg, text) => {
-  if (text.length > 200) {
-    return "😎 I don't read shit that long 😎";
+  if (text.length > 160) {
+    return "😎 I don't read shit that long 😎 160 characters limit.";
+  }
+  const urlSong = await googleTTS(`${msg.member.nickname} said ${text}. end of report. wo`, 'en', 0.6);
+
+  if (!msg.member.voiceChannel){
+    return {
+      files: [urlSong + '.mp3']
+    }
   }
 
   if(isDevChannel(msg.member.voiceChannel)) {
     return;
   }
 
-  const url = await googleTTS(`${msg.member.nickname} said ${text}`, 'en', 1);
+  
+  const songData = await axios({
+    method:'get',
+    url: urlSong,
+    responseType:'stream'
+  });
+
   const conn = await msg.member.voiceChannel.join();
 
-  const dispatch = conn.playFile(url);
-  dispatch.once('end', ()=>{
-    msg.member.voiceChannel.leave();
+  const dispatch = conn.playStream(urlSong);
+  dispatch.once('end', async ()=>{
+    await msg.member.voiceChannel.leave();
   });
 
 }
