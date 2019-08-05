2468257f5fbcce7d387c6d133bc0fbb951e9b69e
Fizal Sarif <fizal.sarif@gmail.com>
2019-08-05 11:43:43 -0400

    add exec

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y sshpass
 sshpass -p $SP ssh -t $SU@$SH -p $SPP \
   '. ~/.nvm/nvm.sh; cd ~/apps/entrybot; git pull origin master; npm i; pm2 restart npm;'
