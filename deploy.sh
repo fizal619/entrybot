028c240bfb678228771372eb984a26a03402bd46
Fizal Sarif <fizal.sarif@gmail.com>
2019-08-05 11:17:32 -0400

    fix deploy source nvm

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 sshpass -p $SP ssh -t $SU@$SH -p $SPP \
-  'cd ~/apps/entrybot; git pull origin master; npm i; pm2 restart npm;'
+  '. ~/.nvm/nvm.sh; cd ~/apps/entrybot; git pull origin master; npm i; pm2 restart npm;'
