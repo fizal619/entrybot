5ac1b960e321d16e2f806d125a8878953dd5813b
Fizal Sarif <fizal.sarif@gmail.com>
2019-08-05 11:33:51 -0400

    sshpass pls

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y sshpass
 sshpass -p $SP ssh -t $SU@$SH -p $SPP \
   '. ~/.nvm/nvm.sh; cd ~/apps/entrybot; git pull origin master; npm i; pm2 restart npm;'
