5e83bfbbb2f39b912c0581ac7cc210916b9799ae
Fizal Sarif <fizal.sarif@gmail.com>
2019-12-02 20:45:27 -0500

    turn off cam

--- routes/kookie.js
+++ routes/kookie.js
@@ -6,23 +6,32 @@ module.exports = async (channel) => {
   try {
     channel.send('Capturing Kookie... 📸');
 
-    ffmpeg(process.env.CAMURL)
-      .duration(5)
-      .size('1280x720')
-      .noAudio()
-      .save('kookie.mp4')
-      .on('error', (e)=>{
-        channel.send(`Something went wrong: ${e.message} 😭.`)
-      })
-      .on('end', ()=> {
-        channel.send('Please open this in your browser.', {
-          files: [{
-            attachment: 'kookie.mp4',
-            name: 'kookie.mp4'
-          }]
-        });
+    // ffmpeg(process.env.CAMURL)
+    //   .duration(5)
+    //   .size('1280x720')
+    //   .noAudio()
+    //   .save('kookie.mp4')
+    //   .on('error', (e)=>{
+    //     channel.send(`Something went wrong: ${e.message} 😭.`)
+    //   })
+    //   .on('end', ()=> {
+    //     channel.send('Please open this in your browser.', {
+    //       files: [{
+    //         attachment: 'kookie.mp4',
+    //         name: 'kookie.mp4'
+    //       }]
+    //     });
+    //   });
+
+    setTimeout(()=>{
+      channel.send('Don\'t fap too hard to this...', {
+        files: [{
+          attachment: 'lola.png',
+          name: 'lola.png'
+        }]
       });
-    // console.log(check.rows);
+    }, 2000);
+
   } catch (e) {
     console.log(`Something went wrong: ${e.message} 😭.`);
     channel.send(`Something went wrong: ${e.message} 😭.`);
