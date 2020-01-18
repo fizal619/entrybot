4462113c9b8403fab5b0b99fc011448c6e50c280
Fizal Sarif <fizal.sarif@gmail.com>
2020-01-18 16:27:26 -0500

    upgrade to patched version - ytdl-core

--- routes/kookie.js
+++ routes/kookie.js
@@ -6,31 +6,31 @@ module.exports = async (channel) => {
   try {
     channel.send('Capturing Kookie... 📸');
 
-    ffmpeg(process.env.CAMURL)
-      .duration(5)
-      .size('1280x720')
-      .noAudio()
-      .save('kookie.mp4')
-      .on('error', (e)=>{
-        channel.send(`Something went wrong: ${e.message} 😭.`)
-      })
-      .on('end', ()=> {
-        channel.send('Please open this in your browser.', {
-          files: [{
-            attachment: 'kookie.mp4',
-            name: 'kookie.mp4'
-          }]
-        });
-      });
-
-    // setTimeout(()=>{
-    //   channel.send('Don\'t fap too hard to this...', {
-    //     files: [{
-    //       attachment: 'lola.png',
-    //       name: 'lola.png'
-    //     }]
+    // ffmpeg(process.env.CAMURL)
+    //   .duration(5)
+    //   .size('1280x720')
+    //   .noAudio()
+    //   .save('kookie.mp4')
+    //   .on('error', (e)=>{
+    //     channel.send(`Something went wrong: ${e.message} 😭.`)
+    //   })
+    //   .on('end', ()=> {
+    //     channel.send('Please open this in your browser.', {
+    //       files: [{
+    //         attachment: 'kookie.mp4',
+    //         name: 'kookie.mp4'
+    //       }]
+    //     });
     //   });
-    // }, 2000);
+
+    setTimeout(()=>{
+      channel.send('Don\'t fap too hard to this...', {
+        files: [{
+          attachment: 'lola.png',
+          name: 'lola.png'
+        }]
+      });
+    }, 2000);
 
   } catch (e) {
     console.log(`Something went wrong: ${e.message} 😭.`);
