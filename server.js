a235382caa984a44bdd7dc3658562b7b662cbcf0
Fizal Sarif <fizal.sarif@gmail.com>
2020-09-12 18:05:37 -0400

    add daily crash lmao

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+cron.schedule('5 7 * * *', () => {
+  console.log('Crashing to combat memory leak lmao.');
+  process.exit();
+});
+
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
