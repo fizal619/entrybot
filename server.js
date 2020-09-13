ad024187b027f5c40fc78e153f98d55d6eeaa9f2
Fizal Sarif <fizal.sarif@gmail.com>
2020-09-12 20:43:30 -0400

    fix cron

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 
 // let's exit the process and force a restart
 // to combat a memory leak I cannot find
-cron.schedule('5 11 * * *', () => {
+cron.schedule('0 0 11 * * *', () => {
   console.log('Crashing to combat memory leak lmao.');
   process.exit();
 });
